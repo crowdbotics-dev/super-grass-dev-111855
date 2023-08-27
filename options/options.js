6aa67c45e08d10d846ef618f5a184348c354aace
Crowdbotics <engineering@crowdbotics.com>
2023-08-27 00:47:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "super-grass-dev-111855",
+  url: "https://super-grass-dev-111855.botics.co",
+  api: "https://super-grass-dev-111855.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
